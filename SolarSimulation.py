4b97f58e079b379b284b5fa4207f06491e8f20fd
sanketpr@buffalo.edu <sanketpr@buffalo.edu>
2020-09-12 17:14:52 -0400

    - Updated the class structure. - Updated and adding methods.

--- SolarSimulation.py
+++ SolarSimulation.py
@@ -1,4 +1,12 @@
-import time
+from time import sleep
+from pubsub import pub
+
+class Water:
+	SpecificHeat = 4.2
+
+	# TODO: Integrate density chart of water for obtaining precise density of water at specific temprature
+	# def getDensity(atTempratuer: int) -> float
+	Density = 999.1		# Kg/m3
 
 class SolarPanel:
 	def __init__(self) :
@@ -6,44 +14,53 @@ class SolarPanel:
 		self.width = 0
 		self.incidentBeam = 0
 
-class HeatingSystem:
-	def __init__(self):
-		self.heatingCoil = HeatingCoil()
-		self.isPumping = False
+	def heatWater(self, volume: int, temprature: int):
+		mass = Water.Density/volume
+		return SolarPanel.heatTransfer(self.incidentBeam, mass, WATER.SpecificHeat, temprature)
 
-	def startPumping(self):
-		return
+	# Calculating increased temprature using Q = mc(dT)
+	def heatTransfer(energy: int, mass: int, specificHeat: float, initialTemp: int) -> int:
+		return (energy/(mass * specificHeat)) + initialTemp
 
-	def stopPumping(self):
-		return
+class PumpingSystem:
+	def __init__(self, panel: SolarPanel, pumpStatus: bool, pumpingRate: int):
+		self.panel = panel
+		self.isPumpActive = False
+		self.pumpingRate = 0		# Kg per second
 
-class HeatingCoil:
-	def __init__(self):
-		self.temp = 0
-		self.isInSteadyState = False
+	def setIsPumpActive(self, status):
+		self.isPumpActive = status
 
-	# Amount of heat exchange per min
-	def descipateHeate(self) -> int:
-		return
+	def setPumpingRate(self, rate):
+		self.pumpingRate = rate
 
-	def heatup(self):
-		return
+	def pumpInPanel(self, temprature: int) -> int:
+		self.panel.heatWater(self.pumpingRate, temprature)
 
 class Tank:
+	def __init__(self, capacity: int, waterCap: int):
+		self.capacity = capacity	# Volume of tank in Liters
+		self.waterCap = waterCap	# Volume of water in Liters
+		self.waterTemp = 0			# Uniform water temprature in C
+
+	# TODO: Should be able to fill the tank and overflow the extra water. Is it a valid approach?
+	def addHotWater(self, volume: int, temprature: int):
+		if(volume > self.capacity - self.waterCap):
+			print("Cannot add more water than the overall tank capacity")
+		self.mixWater(volume, temprature)
+
+	def mixWater(self,volume, temprature):
+		self.waterTemp = ((volume*temprature)+(self.waterTemp*self.waterCap))/(volume+self.waterCap)
+		self.waterCap += volume
+
+	def releaseWater(self, volume):
+		if amount > self.waterCap:
+			print("Sepcify volume not more than current volume of water\nCurrent Volume of Water:",self.waterCap," Kg/m3")
+			return
+		self.waterCap -= volume
+
+class Controller:
 	def __init__(self):
-		self.height = 0
-		self.width = 0
-		self.depth = 0
-		self.heatingCoil = HeatingCoil()
-		self.waterTemp = 0
 
-	def heatWater(self):
-		while(self.heatingCoil.isInSteadyState):
-			self.waterTemp += self.heatingCoil.descipateHeate(self.waterTemp)
-			time.sleep(60)
-		return
-
-	def releaseHotWater(self, amount):
-		return
 
 def main():
